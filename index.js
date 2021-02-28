994e648f5475b7cbbbb7334c3371c0176d23406a
Micah Banschick <micha.ban@gmail.com>
2021-02-28 22:54:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // Your code goes here
 document.addEventListener("DOMContentLoaded", function() {
   console.log("The DOM has loaded");
-  console.log('This is really cool!')
+  console.log('This is really cool!');
+  return 'This is really cool!'
 });
 
 console.log(
